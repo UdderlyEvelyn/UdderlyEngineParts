cb1e979d14daf08d0afbbeb3cd2a1567cdf075b8
UdderlyEvelyn <udderlyevelyn@gmail.com>
2022-07-31 19:59:15 -0400

    initial commit

---
+++ Contents/mods/UdderlyEngineParts/media/lua/server/ItemDistributions.lua
@@ -0,0 +1,18 @@
+require 'Items/ProceduralDistributions'
+
+table.insert(ProceduralDistributions.list["BookstoreBooks"].items, "Base.Magazine_UdderlyEngineParts");
+table.insert(ProceduralDistributions.list["BookstoreBooks"].items, 1);
+table.insert(ProceduralDistributions.list["CrateMagazines"].items, "Base.Magazine_UdderlyEngineParts");
+table.insert(ProceduralDistributions.list["CrateMagazines"].items, 1);
+table.insert(ProceduralDistributions.list["CrateMechanics"].items, "Base.Magazine_UdderlyEngineParts");
+table.insert(ProceduralDistributions.list["CrateMechanics"].items, 1);
+table.insert(ProceduralDistributions.list["LibraryBooks"].items, "Base.Magazine_UdderlyEngineParts");
+table.insert(ProceduralDistributions.list["LibraryBooks"].items, 1);
+table.insert(ProceduralDistributions.list["MagazineRackMixed"].items, "Base.Magazine_UdderlyEngineParts");
+table.insert(ProceduralDistributions.list["MagazineRackMixed"].items, 0.5);
+table.insert(ProceduralDistributions.list["MechanicShelfBooks"].items, "Base.Magazine_UdderlyEngineParts");
+table.insert(ProceduralDistributions.list["MechanicShelfBooks"].items, 1);
+table.insert(ProceduralDistributions.list["MechanicShelfBooks"].junk.items, "Base.Magazine_UdderlyEngineParts");
+table.insert(ProceduralDistributions.list["MechanicShelfBooks"].junk.items, 1);
+table.insert(ProceduralDistributions.list["ShelfGeneric"].items, "Base.Magazine_UdderlyEngineParts");
+table.insert(ProceduralDistributions.list["ShelfGeneric"].items, 0.1);
